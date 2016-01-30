24bd4f2c385e73443fb308f63de069061fc7654d
Sam Broderick <waveman68@gmail.com>
2016-01-30 12:10:18 +0100

    Synch

--- L6_problems.py
+++ L6_problems.py
@@ -1,5 +1,6 @@
 __author__ = 'Sam Broderick'
 
+
 def oddTuples(aTup):
     """
     aTup: a tuple
@@ -8,10 +9,13 @@ def oddTuples(aTup):
     :param aTup: tuple
     """
     # Your Code Here
+    if aTup == ():
+        return aTup
     return_tuple = (aTup[0], )
     for n in range(2, len(aTup), 2):
         return_tuple = return_tuple + (aTup[n], )
 
     return return_tuple
 
-
+print(oddTuples(('I', 'am', 'a', 'test', 'tuple')))
+print(oddTuples(()))
