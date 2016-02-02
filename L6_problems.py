325299a66241e04dc884afff042bea849fd7d04e
Sam Broderick <waveman68@gmail.com>
2016-02-02 11:13:42 +0100

    Backup

--- L6_problems.py
+++ L6_problems.py
@@ -52,3 +52,27 @@ print(testList)
 testList = [1, -4, 8, -9]
 applyToEach(testList, square)
 print(testList)
+
+print('=============')
+
+
+def applyEachTo(L, x):
+    result = []
+    for i in range(len(L)):
+        result.append(L[i](x))
+    return result
+
+
+def square(a):
+    return a*a
+
+
+def halve(a):
+    return a/2
+
+
+def inc(a):
+    return a+1
+
+print(applyToEach([inc, square, halve, abs], -3))
+print(applyEachTo([inc, square, halve, abs], 3.0))
