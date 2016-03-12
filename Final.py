cff36de85580dec015b6d824f7df73b8a8da8e7e
waveman68 <sbroderick@mac.com>
2016-03-12 11:14:49 +0100

    Sync code for final.

--- Final.py
+++ Final.py
@@ -74,3 +74,58 @@ def longestRun(L):
             return n
 
 print(longestRun(L1))
+
+
+class Person(object):
+    def __init__(self, name):
+        self.name = name
+    def say(self, stuff):
+        return self.name + ' says: ' + stuff
+    def __str__(self):
+        return self.name
+
+class Lecturer(Person):
+    def lecture(self, stuff):
+        return 'I believe that ' + Person.say(self, stuff)
+
+class Professor(Lecturer):
+    def say(self, stuff):
+        return self.name + ' says: ' + self.lecture(stuff)
+
+class ArrogantProfessor(Professor):
+    def say(self, stuff):
+        return Person.say(self, self.lecture(stuff))
+    def lecture(self, stuff):
+        return 'It is obvious that ' + Person.say(self, stuff)
+
+e = Person('eric') 
+le = Lecturer('eric') 
+pe = Professor('eric') 
+ae = ArrogantProfessor('eric')
+
+print('======')
+print(e.say('the sky is blue'))
+# print('eric says: the sky is blue')
+
+print('======')
+print(le.say('the sky is blue'))
+# print('eric says: the sky is blue')
+
+print('====== le ======')
+print(le.lecture('the sky is blue'))
+# print('I believe that eric says: the sky is blue')
+
+print('====== pe ======')
+print(pe.say('the sky is blue'))
+# eric says: I believe that eric says: the sky is blue
+
+print('======')
+print(pe.lecture('the sky is blue'))
+# I believe that eric says: the sky is blue
+
+print('====== ae ======')
+print(ae.say('the sky is blue'))
+print('eric says: It is obvious that eric says: the sky is blue')
+
+print(ae.lecture('the sky is blue'))
+print('It is obvious that eric says: the sky is blue')
